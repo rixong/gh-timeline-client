d0e92edf2591a2498852c5ab07da56ebea308361
mattjagiello <mjagiello@gmail.com>
2020-04-13 15:55:21 -0500

    Removed click to see profile button

--- src/components/Login.js
+++ src/components/Login.js
@@ -84,8 +84,6 @@ handleLoginSubmit = (e) => {
         <h2>About Github Timeline App</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
-<br></br>
-      <button onClick={this.handleClick}>Click to see profile info</button>
     </div>
   }
 }
