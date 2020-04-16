c400abedeead45c7fa0125c71308e661ca246ecc
mattjagiello <mjagiello@gmail.com>
2020-04-15 23:19:49 -0500

    Uncommented selection change

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -36,10 +36,10 @@ class Profile extends Component {
     return this.state.timelines.map((item, idx) => <option key={idx} value={item.name} >{item.name}</option>)
   }
 
-  // handleSelectionChange = (e) => {
-  //   console.log(e.target.value);
-  //   this.setState({ searchTerm: e.target.value })
-  // }
+  handleSelectionChange = (e) => {
+    console.log(e.target.value);
+    this.setState({ searchTerm: e.target.value })
+  }
 
   handleChange = (e) => {
     this.setState({
